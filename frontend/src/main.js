7fc332e30ff61ac043d1e5fa4116abdcdeb2f551
Mictronics <github@mictronics.de>
2025-12-13 22:21:23 +0100

    Added Airbus color scheme.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.min.css';
 import './assets/app.css';
 import { createApp } from 'vue';
+import { definePreset, palette } from '@primeuix/themes';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
 import Lara from '@primeuix/themes/lara';
@@ -16,10 +17,63 @@ import Dialog from 'primevue/dialog';
 import Toast from 'primevue/toast';
 import ToastService from 'primevue/toastservice';
 
+const Airbus = definePreset(Lara, {
+  semantic: {
+    primary: {
+      0: '#ffffff',
+      50: '#f2f4f7',
+      100: '#c2c9d8',
+      200: '#919fb8',
+      300: '#617599',
+      400: '#304a7a',
+      500: '#00205b',
+      600: '#001b4d',
+      700: '#001640',
+      800: '#001232',
+      900: '#000d24',
+      950: '#000817'
+    },
+    colorScheme: {
+      light: {
+        surface: {
+          0: '#ffffff',
+          50: '#f2f4f7',
+          100: '#c2c9d8',
+          200: '#919fb8',
+          300: '#617599',
+          400: '#304a7a',
+          500: '#00205b',
+          600: '#001b4d',
+          700: '#001640',
+          800: '#001232',
+          900: '#000d24',
+          950: '#000817'
+        }
+      },
+      dark: {
+        surface: {
+          0: '#ffffff',
+          50: '#f2f4f7',
+          100: '#c2c9d8',
+          200: '#919fb8',
+          300: '#617599',
+          400: '#304a7a',
+          500: '#00205b',
+          600: '#001b4d',
+          700: '#001640',
+          800: '#001232',
+          900: '#000d24',
+          950: '#000817'
+        }
+      }
+    }
+  }
+});
+
 const app = createApp(App);
 app.use(PrimeVue, {
   theme: {
-    preset: Lara,
+    preset: Airbus,
     options: {
       darkModeSelector: '.my-app-dark'
     }
